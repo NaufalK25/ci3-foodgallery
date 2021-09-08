12ee69e946b0fa4aaccf43a238f45ca66e0ce8e6
NaufalK25 <naufalkateni2001@gmail.com>
2021-09-08 16:36:53 +0700

    Add base url to constant

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -2,9 +2,9 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<link rel="stylesheet" href="<?= base_url(); ?>assets/css/error_404.css">
-<link rel="stylesheet" href="<?= base_url(); ?>assets/css/templates/navbar.css">
-<link rel="stylesheet" href="<?= base_url(); ?>assets/css/templates/footer.css">
+<link rel="stylesheet" href="<?= BASEURL ?>assets/css/error_404.css">
+<link rel="stylesheet" href="<?= BASEURL ?>assets/css/templates/navbar.css">
+<link rel="stylesheet" href="<?= BASEURL ?>assets/css/templates/footer.css">
 </head>
 <body>
 
@@ -15,19 +15,19 @@
 <div class="d-flex justify-content-center">
 	
 	<div class="error-kotak">
-		<img src="<?= base_url(); ?>assets/img/person-with-empty-plate.png" alt="No Food">
+		<img src="<?= BASEURL ?>assets/img/person-with-empty-plate.png" alt="No Food">
 		<div class="error-tulisan">
 			<p class="fs-3">Tidak Ada Makanan Disini!</p>
 			<?php if(!$this->session->username): ?>
 				<p>
 					Kembali ke
-					<a href="<?= base_url(); ?>home" class="error-link">Home</a>
+					<a href="<?= BASEURL ?>home" class="error-link">Home</a>
 				</p>
 			<?php else: ?>
 				<p>
 					Kembali ke?
-					<a href="<?= base_url(); ?>home" class="error-link">Home</a> |
-					<a href="<?= base_url(); ?>recipe-list/1" class="error-link">Daftar Resep</a>
+					<a href="<?= BASEURL ?>home" class="error-link">Home</a> |
+					<a href="<?= BASEURL ?>recipe-list/1" class="error-link">Daftar Resep</a>
 				</p>
 			<?php endif; ?>
 		</div>
